8929f28af5c6c0d1822c6aeee00d77700cb8e56f
Suraj Prabhu <surajprabhu19@gmail.com>
2024-05-17 15:18:07 +0530

    Added Components + Routes + updated app.component.html + app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, MatIconModule, MatButtonModule, MatToolbarModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from '../components/dashboard/dashboard.component';
+import { SubjectsComponent } from '../components/subjects/subjects.component';
+import { AllStudentsComponent } from '../components/all-students/all-students.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "/dashboard",
+        component: DashboardComponent
+    },
+    {
+        path: "/subjects",
+        component: SubjectsComponent
+    },
+    {
+        path: "/students",
+        component: AllStudentsComponent
+    }
+];
