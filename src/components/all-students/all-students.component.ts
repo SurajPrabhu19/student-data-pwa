61a886264dedbdb68672bd3494e47c91c5311502
Suraj Prabhu <surajprabhu19@gmail.com>
2024-05-21 01:54:42 +0530

    added html and .ts logic for allStudents

--- src/components/all-students/all-students.component.ts
+++ src/components/all-students/all-students.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { } from '@angular/material/';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
+import { StudentService } from '../services/student-service/student.service';
 
 @Component({
   selector: 'app-all-students',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, MatIconModule, MatCardModule, MatDividerModule],
   templateUrl: './all-students.component.html',
   styleUrl: './all-students.component.scss'
 })
 export class AllStudentsComponent {
 
+  students: any = [{ name: "a" }, { name: "b" }]
+  /**
+   *
+   */
+  constructor(
+    private studentService: StudentService
+  ) {
+    this.studentService.getAllStudents().subscribe(
+      (res: any) => {
+        this.students = res;
+      }
+    )
+  }
 }
