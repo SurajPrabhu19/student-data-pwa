bc278feb1a8d3182894a1cd27972bba77bb96220
Suraj Prabhu <surajprabhu19@gmail.com>
2024-05-21 02:03:46 +0530

    Added studentService in root

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { RouterOutlet } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { StudentService } from '../components/services/student-service/student.service';
 
 
 @Component({
@@ -12,7 +13,8 @@ import { MatToolbarModule } from '@angular/material/toolbar';
   standalone: true,
   imports: [RouterOutlet, CommonModule, MatIconModule, MatButtonModule, MatToolbarModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  providers: [StudentService]
 })
 export class AppComponent {
   title = 'student-data-pwa';
