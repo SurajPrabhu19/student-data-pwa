72d828ac78d9265d4208fb9f3ecaa972288a8ec8
Suraj Prabhu <surajprabhu19@gmail.com>
2024-05-21 02:02:58 +0530

    Updated dashboard comp with <app-user>

--- src/components/dashboard/dashboard.component.ts
+++ src/components/dashboard/dashboard.component.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
+import { UserComponent } from '../user/user.component';
+import { StudentService } from '../services/student-service/student.service';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [HttpClientModule, RouterModule],
+  imports: [HttpClientModule, RouterModule, UserComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
 
+  teacher: any;
+
+  constructor(
+    private studentService: StudentService
+  ) {
+    this.studentService.getTeachersProfile().subscribe(
+      (res:any) => {
+        this.teacher = res;
+      }
+    )
+  }
 }
