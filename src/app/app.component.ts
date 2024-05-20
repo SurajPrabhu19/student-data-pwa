eb58ff45c42033aa01dd9313978644e5ac6030a2
Suraj Prabhu <surajprabhu19@gmail.com>
2024-05-21 02:16:21 +0530

    Passing value from Dashboard to User Comp using Input()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,17 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { StudentService } from '../components/services/student-service/student.service';
+import { HttpClientModule } from '@angular/common/http';
+import { UserComponent } from '../components/user/user.component';
+import { SubjectsComponent } from '../components/subjects/subjects.component';
+import { AllStudentsComponent } from '../components/all-students/all-students.component';
+import { DashboardComponent } from '../components/dashboard/dashboard.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, MatIconModule, MatButtonModule, MatToolbarModule],
+  imports: [RouterOutlet, CommonModule, MatIconModule, MatButtonModule, MatToolbarModule, HttpClientModule, UserComponent, SubjectsComponent, AllStudentsComponent, DashboardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   providers: [StudentService]
--- src/components/dashboard/dashboard.component.ts
+++ src/components/dashboard/dashboard.component.ts
@@ -1,26 +1,26 @@
 import { Component } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { UserComponent } from '../user/user.component';
 import { StudentService } from '../services/student-service/student.service';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [HttpClientModule, RouterModule, UserComponent],
+  imports: [HttpClientModule, RouterModule, UserComponent, RouterOutlet],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
 
-  teacher: any;
+  teacherProfile: any;
 
   constructor(
     private studentService: StudentService
   ) {
     this.studentService.getTeachersProfile().subscribe(
       (res:any) => {
-        this.teacher = res;
+        this.teacherProfile = res;
       }
     )
   }
